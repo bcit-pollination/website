c39d81b67f60db42e449ecdbbbe645faa754bb2f
Hans Sy <55111820+hanssy130@users.noreply.github.com>
2021-02-09 23:35:24 -0800

    Added EditOrganization.js component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import Header from './components/Header'
 import Button from 'react-bootstrap/Button';
 import RegistrationForm from './components/RegistrationForm';
 import LoginForm from './components/LoginForm';
-import AlertComponent from './components/AlertMessage';  
-import Home from './screens/Home';  
+import EditOrganization from './components/EditOrganization';
+import AlertComponent from './components/AlertMessage';
+import Home from './screens/Home';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
-import {useState} from 'react';
+import { useState } from 'react';
 
 
 function App() {
@@ -20,28 +21,31 @@ function App() {
 
   return (
     <Router>
-    <div className="App">
-      <Header className="App-header" title={title} onBackClick={() => {console.log("TAKE ME BACK")}}/>
-      {/* <RegistrationForm onClick={() => {console.log("onClick passed from App.js")}}/> */}
+      <div className="App">
+        <Header className="App-header" title={title} onBackClick={() => { console.log("TAKE ME BACK") }} />
+        {/* <RegistrationForm onClick={() => {console.log("onClick passed from App.js")}}/> */}
 
-      <div className="container d-flex align-items-center flex-column">
+        <div className="container d-flex align-items-center flex-column">
           <Switch>
             <Route path="/" exact={true}>
-              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle} />
             </Route>
             <Route path="/register">
-              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle} />
             </Route>
             <Route path="/login">
-              <LoginForm showError={updateErrorMessage} updateTitle={updateTitle} onClick={() => {console.log("onClick passed from App.js")}}/>
+              <LoginForm showError={updateErrorMessage} updateTitle={updateTitle} onClick={() => { console.log("onClick passed from App.js") }} />
             </Route>
             <Route path="/home">
-              <Home/>
+              <Home />
+            </Route>
+            <Route path="/editOrganization">
+              <EditOrganization showError={updateErrorMessage} updateTitle={updateTitle} />
             </Route>
           </Switch>
-          <AlertComponent errorMessage={errorMessage} hideError={updateErrorMessage}/>
+          <AlertComponent errorMessage={errorMessage} hideError={updateErrorMessage} />
         </div>
-    </div>
+      </div>
     </Router>
   );
 }
