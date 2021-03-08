05bb8368eff6bce04f23c52f03eb60b25b64a79f
TrungYU <quoctrung1220@gmail.com>
2021-03-08 03:32:17 -0800

    org creation and org list

--- src/screens/CreateOrganization.js
+++ src/screens/CreateOrganization.js
@@ -1,39 +1,63 @@
 import '../css/App.css'
 import { withRouter } from "react-router-dom";
 import { useForm } from 'react-hook-form';
+import { postReqA } from '../utils/customAxiosLib';
 
 const CreateOrganizationForm = (props) => {
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => {
-        console.log(data);
-    };
 
-    const leftForm = { position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', minHeight: '70vh' }
+    const { register, handleSubmit } = useForm();
+
+    let jwt_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJwb2xsaW5hdGlvbi5saXZlIiwiaWF0IjoxNjE1MjAwNzI5LCJleHAiOjE2MjEyMDA3MjksInVpZCI6IjUifQ.R757PBDilIYsmO_UzLo5VpqBq9fyVqaHbyJHzYilzpQ";
+
+    const onSubmit = data => {
+        console.log(data.orgName);
+
+        postReqA('/org', jwt_token, {
+            "name":data.orgName,
+            "user_org_id":"20021998",  // TODO add UI
+            "verifier_password":"20021998"  // TODO add UI
+        })
+        .then(response => {
+            if (response.status === 200) {
+                console.log("org created!!!")
+            }
+        })
+        .catch(error => {
+            console.log("Create org failed: ");
+            console.log(error);
+        })
+    };
 
     const newLine = "\r\n";
     const emailPlaceholderText = "{ " + newLine + "  EMAIL 1: UID," + newLine + "  EMAIL 2: UID," + newLine + "  EMAIL 3: UID," + newLine + "}"
+
+    const centerForm = {position:'absolute', top:'50%', left:'50%', transform: 'translate(-50%, -50%)', minHeight:'50vh' }
+
     return (
         <div className="CreateOrg-form">
-            <div className="card col-12 col-lg-4 mt-2 hv-center" style={leftForm}>
+            <div className={`card col-12 col-lg-4 mt-2 hv-center`} style={centerForm}>
                 <form onSubmit={handleSubmit(onSubmit)}>
+                   
                     <h2>Create Organization</h2>
+                   
                     <div className="form-group text-left">
                         <label htmlFor="orgName">Name</label>
                         <input type="text"
                             className="form-control"
-                            id="orgname"
-                            name="orgNAme"
-                            placeholder="//TODO ORGANIZATION NAME"
+                            id="orgName"
+                            name="orgName"
+                            placeholder="Org Name"
                             ref={register}
                             required="required"
                         />
                     </div>
+
                     <div className="form-group text-left">
                         <label htmlFor="emailList">Emails</label>
                         <textarea type="scrolalbletextbox"
                             className="form-control"
                             cols="1"
-                            id="elist"
+                            id="emailList"
                             name="emailList"
                             placeholder={emailPlaceholderText}
                             ref={register}
@@ -41,10 +65,11 @@ const CreateOrganizationForm = (props) => {
                             required="required"
                         />
                     </div>
-                    <input type="submit" value="Update" />
+                    
+                    <input type="submit" value="Create" />
                     <br />
                     <br />
-                    <input type="submit" value="Cancel" />
+                    {/* <input type="submit" value="Cancel" /> */}
                 </form>
             </div>
         </div>
--- src/utils/customAxiosLib.js
+++ src/utils/customAxiosLib.js
@@ -3,20 +3,22 @@ import axios from 'axios';
 
 const api_path = 'http://pollination.live/api';
 
+const logResponse = (response) => {
+    console.log("Returned from server:");
+    console.log("Response Status:");
+    console.log(response.status);
+    console.log("Response statusText:");
+    console.log(response.statusText);
+    console.log("Response headers:");
+    console.log(response.headers);
+    console.log("Response data:");
+    console.log(response.data);
+}
+
 export const getReq = async (apiEndpoint) => {
     try {
         const response = await axios.get(api_path + apiEndpoint);
-
-        console.log("Returned from server:");
-        console.log("Response Status:");
-        console.log(response.status);
-        console.log("Response statusText:");
-        console.log(response.statusText);
-        console.log("Response headers:");
-        console.log(response.headers);
-        console.log("Response data:");
-        console.log(response.data);
-        
+        logResponse(response);
         return response;
     } catch (error) {
         // Handle Error Here
@@ -28,16 +30,54 @@ export const getReq = async (apiEndpoint) => {
 export const postReq = async (apiEndpoint, jsonObj) => {
     try {
         const response = await axios.post(api_path + apiEndpoint, jsonObj);
+        logResponse(response);
+        return response;
+    } catch (error) {
+        // Handle Error Here
+        console.log("postReq: " + error);
+        return error;
+    }
+}
 
-        console.log("Returned from server:");
-        console.log("Response Status:");
-        console.log(response.status);
-        console.log("Response statusText:");
-        console.log(response.statusText);
-        console.log("Response headers:");
-        console.log(response.headers);
-        console.log("Response data:");
-        console.log(response.data);
+export const getReqA = async (apiEndpoint, jwt_token) => {
+    try {
+        const response = await axios.get(
+            api_path + apiEndpoint,
+            { headers: {
+                "Authorization" : `Bearer ${jwt_token}`,
+            }
+        });
+        logResponse(response);
+        return response;
+    } catch (error) {
+        // Handle Error Here
+        console.log("getReq: " + error);
+        return error;
+    }
+}
+
+export const postReqA = async (apiEndpoint, jwt_token, jsonObj) => {
+    try {
+        console.log(jwt_token);
+        // let axiosInstance =  axios.create({
+        //     baseURL: api_path,
+        //     headers: {
+        //         'Authorization': jwt_token,
+        //         'Content-Type': 'application/json'
+        //     }
+        // }); 
+
+        // const response = await axiosInstance.post(apiEndpoint,{jsonObj});
+
+        const response = await axios.post(
+            api_path + apiEndpoint, 
+            jsonObj, 
+            { headers: {
+                "Authorization" : `Bearer ${jwt_token}`,
+            } 
+        });
+
+        logResponse(response);
 
         return response;
     } catch (error) {
