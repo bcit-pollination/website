2b552be9bbeab8de70ec6d402981329843a479ff
allen <awong503@my.bcit.ca>
2021-03-28 23:08:09 -0700

    Add table and data to electionResults

--- src/screens/ElectionResults.js
+++ src/screens/ElectionResults.js
@@ -1,13 +1,52 @@
 import '../css/App.css'
 import { 
     withRouter, 
-    useParams
+    Switch, 
+    Route,
+    useRouteMatch
 } from "react-router-dom";
 import { getReq } from '../utils/customAxiosLib'
 import { useState, useEffect} from 'react';
 
+const renderTableHeader = () => {
+    return (
+        <tr>
+            <th key={0}>ID</th>
+            <th key={1}>ELECTION DESCRIPTION</th>
+            <th key={2}>START TIME</th>
+            <th key={3}>END TIME</th>
+            <th key={5}>ORG ID</th>
+        </tr>
+    );
+}
+
+const renderTableData = (electionList) => {
+    return electionList.map((election, index) => {
+        const {election_description, election_id, end_time,
+        start_time, org_id} = election;
+        return (
+        <tr 
+        key={index}
+        >
+            <td>{election_id}</td>
+            <td>{election_description}</td>
+            <td>{start_time}</td>
+            <td>{end_time}</td>
+            <td>{org_id}</td>
+        </tr>
+        );
+    });
+}
+
 const ElectionResults = (props) => {
-    const [listState, setState] = useState();
+    const [listState, setState] = useState([{
+        anonymous : "",
+        election_description: "",
+        election_id: 0,
+        end_time: "",
+        start_time: "",
+        ord_id: 0
+    }]);
     useEffect(() => {
         getReq("/org/elections/public/get/list?page=1&elections_per_page=333").then(response => {
             if (response.status === 200) {
@@ -20,9 +59,18 @@ const ElectionResults = (props) => {
     }, []);
     
     console.log(listState);
+
+
     return (
-        <>
-        </>
+        <div>
+            <h2 className='title'>Public Elections List</h2>
+            <table id="org">
+                <tbody>
+                    {renderTableHeader()}
+                    {renderTableData(listState)}
+                </tbody>
+            </table>
+        </div>
     );
 }
 export default withRouter(ElectionResults);
