b5d12fbb6145c3742fe95d949b2e19196872d902
allen <awong503@my.bcit.ca>
2021-03-28 23:19:44 -0700

    Add routing

--- src/screens/ElectionResults.js
+++ src/screens/ElectionResults.js
@@ -20,13 +20,16 @@ const renderTableHeader = () => {
     );
 }
 
-const renderTableData = (electionList) => {
+const renderTableData = (electionList, redirectToElectionDetails) => {
     return electionList.map((election, index) => {
         const {election_description, election_id, end_time,
         start_time, org_id} = election;
         return (
         <tr 
         key={index}
+        onClick={() => {
+            redirectToElectionDetails(election_id, election_description)
+        }}
         >
             <td>{election_id}</td>
             <td>{election_description}</td>
@@ -39,6 +42,10 @@ const renderTableData = (electionList) => {
 }
 
 const ElectionResults = (props) => {
+    const redirectToElectionDetails = (id, name) => {
+        console.log("[ + ] Redirecting to view details of " + name);
+        props.history.push(`/electionResults/elecitonDetails/${id}`);
+    }
     const [listState, setState] = useState([{
         anonymous : "",
         election_description: "",
@@ -58,19 +65,25 @@ const ElectionResults = (props) => {
         });
     }, []);
     
-    console.log(listState);
-
+    let { path } = useRouteMatch();
 
     return (
+        <Switch>
+        <Route exact path={path}>
         <div>
             <h2 className='title'>Public Elections List</h2>
             <table id="org">
                 <tbody>
                     {renderTableHeader()}
-                    {renderTableData(listState)}
+                    {renderTableData(listState, redirectToElectionDetails)}
                 </tbody>
             </table>
-        </div>
+        </div>   
+        </Route>
+        {/* <Route path={`/electionResults/electionDetails/:electionId`}>
+
+        </Route> */}
+        </Switch>
     );
 }
 export default withRouter(ElectionResults);
